6eb4e20e469c979122ed15b4fbc236544b702052
IroncloneX <97147753+IroncloneX@users.noreply.github.com>
2022-03-19 05:57:33 +0200

    Update MovieInfo.js

--- libs/MovieInfo.js
+++ libs/MovieInfo.js
@@ -27,13 +27,16 @@ var date = s.Released
 var due = s.Runtime
 var imdbid = s.imdbID
 var imdbr = s.imdbRating
+var aimdbr = s.imdbRating
 var votes = s.imdbVotes
+var meta = s.Metascore
+var box = s.BoxOffice
 var lang = s.Language
 var country = s.Country
 var writer = s.Writer
 var actor = s.Actors
 var year = s.Year
-if(option.imdbbot==true){
+if(options.imdbbot==true){
 Api.sendMessage({
   text: "<b><a href='" + pic + "'>Title</a></b>: <a href='https://www.imdb.com/title/" + imdbid + "'>"+ name + "</a> [" + year + "]\n<i>Also known as</i>: "+ name + " (" + year + ")\n<b>Rating</b>🌟: <i>" + imdbr +  "/10</i>\n(<code>" + imdbr + " based on " + votes + " user ratings</code>)| <code>" + rate + "</code> | <code>" + due + "</code> |\n<b>Release Info</b>: <a href='https://www.imdb.com/title/" + imdbid + "/releaseinfo'>" + date + "</a>\n<b>Genre</b>🎭: " + genre + "\n<b>IMDB url</b>: https://www.imdb.com/title/" + imdbid + "\n<b>Language</b>: " + lang + "\n<b>Country of origin</b>: " + country + "\n<b>Story line</b>: " + des + "\n<b>Writers</b>: " + writer + "\n<b>Stars</b>: " + actor + "\n\n<a href='https://www.imdb.com/title/" + imdbid + "/fullcredits#cast'>Read More...</a>",
   parse_mode: "HTML"
@@ -41,7 +44,7 @@ Api.sendMessage({
 }else{
 var med = "<b>Title</b>: <u>" + name + "</u>\n<b>Release Date</b>: " + date +"\n<b>Genres</b>: " + genre + "\n<b>Rated</b>: " + rate + "\n<b>Duration</b>: " + due + "\n\n<b><u>Description</u></b>: \n" + des + "\n\n<b>Writer</b>: " + writer + "\n<b>Actors</b>: " + actor + "\n<b><a href='https://www.imdb.com/title/" + imdbid + "'>IMDB </a>Rating</b>: " + imdbr + "/10 \n<b>Votes</b>: " + votes + "\n<b>Metacritic</b>: " + meta + "/100\n<b>Box Office</b>: " + box + "\n<b>Language</b>: " + lang + "\n<b>Country</b>: " + country
 Api.sendPhoto({
-  photo: picture(pic),
+  photo: "https://www.imdb.com/title/" + imdbid ,
   caption: med,
   parse_mode: "HTML"
 })
@@ -56,8 +59,7 @@ var boxot = decodeURI(User.getProperty("MovieInfo"))
 if(response=="False"){
   Api.sendMessage({
   text: "<i>No results were found that included all of your search.</i>\n\nYour search -  "+boxot+ " -  did not find any movie.\n\n<b>Server message</b>: \n" + error,
-  parse_mode: "HTML",
-  reply_markup: {inline_keyboard: button}
+  parse_mode: "HTML"
 })
 }else{
 im(options, s)
